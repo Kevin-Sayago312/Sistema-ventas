26dc907387b637d745223de8a8a3e8b758db163c
Kevin Sayago <120409494+Kevin-Sayago312@users.noreply.github.com>
2025-02-13 17:19:41 -0600

    Commit de prueba
    Commit de prueba en la plantilla WELCOME

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
             <!---Marca navegación-->
             <a class="navbar-brand" href="{{route('panel')}}">
                 <img src="{{ asset('assets/img/icon.png') }}" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
-                Sistema de venta
+                Sistema de venta XD
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
